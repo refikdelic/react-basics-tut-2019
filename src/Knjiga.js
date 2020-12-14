c39f316bb845cfe17ed650e67a2141cec6c9c820
refikdelic <59886785+refikdelic@users.noreply.github.com>
2020-12-14 16:59:36 +0100

    vjezbica

--- src/Knjiga.js
+++ src/Knjiga.js
@@ -19,9 +19,9 @@ const Knjiga = (props) => {
  <h3 onMouseOver={()=>console.log(naslov) }>{naslov}</h3>
  <h5>{autor}</h5>
 
-<button className='batn' type='button' onClick={klik}>Hajklikni</button>
+<button className='batn' type='button' onClick={klik}>Haj klikni</button>
 
-<button type='button' onClick={() => complexan(autor)}>Konzzzzola</button>
+<button type='button' onClick={() => complexan(autor)}>Kon zz zz ola</button>
 
    </article>
 
